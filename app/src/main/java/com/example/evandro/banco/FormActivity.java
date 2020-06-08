83f671395357b3a18f53db61cda3ece100b26cf4
evandro_gdn <evandrogardolinn@gmail.com>
2020-06-08 20:20:47 -0300

    [ADD] #activities - Adicionando as activities referentes aos formularios em questao

---
+++ app/src/main/java/com/example/evandro/banco/FormActivity.java
@@ -0,0 +1,6 @@
+package com.example.evandro.banco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/evandro/banco/WithdrawActivity.java
@@ -0,0 +1,6 @@
+package com.example.evandro.banco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WithdrawActivity extends AppCompatActivity {
+}

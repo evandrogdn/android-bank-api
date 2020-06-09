48b84618007f76c6f4f908bd22502f56ddbc1f8a
evandro_gdn <evandrogardolinn@gmail.com>
2020-06-09 20:05:42 -0300

    [ADD] #models - Adicionados model referente ao objeto Endereco

---
+++ app/src/main/java/com/example/evandro/banco/models/Endereco.java
@@ -0,0 +1,60 @@
+package com.example.evandro.banco.models;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
